a839d4bc1aacfcf62a1fc9b10f8beec7098d8133
IanMacwan <ianmacwan11@gmail.com>
2026-06-04 22:34:22 -0400

    updated treesitter config

--- nvim/lua/i/plugins/nvim-treesitter.lua
+++ nvim/lua/i/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local configs = require("nvim-treesitter")
 
 	      	configs.setup({
-		ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "rust", "go", "cpp", "javascript", "html", "elixir", "heex", "eex", "markdown", "markdown_inline" },
+		ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "rust", "go", "cpp", "javascript", "html", "elixir", "heex", "eex", "markdown", "markdown_inline", "typescript", "jsx", "tsx" },
 		sync_install = false,
 		highlight = {
         enable = true,
